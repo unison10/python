e0c7c52a16e4bff695b8dfdb101683170c4da057
unison10 <unison1162@gmail.com>
2015-06-10 15:49:08 +0530

    file has been modified

--- methods.py
+++ methods.py
@@ -1,7 +1,10 @@
-def move(time,speed):
-	print("i am moving on "+ str(speed) + "kmph" + "at" + str(time))
-	x=time+1
-	y=speed-1
-	print("i am moving on "+ str(x) + "kmph" + "at" + str(y))
+#def move(time,speed):
+#	print("i am moving on "+ str(speed) + "kmph" + "at" + str(time))
+#	x=time+1
+#	y=speed-1
+#	print("i am moving on "+ str(x) + "kmph" + "at" + str(y))
 
-move(10,5)
+#move(10,5)
+
+
+print ("hello")
